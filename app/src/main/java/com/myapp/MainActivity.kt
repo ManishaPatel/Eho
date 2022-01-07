30867b7635d7e225b32700bd82f5d657b3f59ddd
KhushwantPanchal21 <94053364+KhushwantPanchal21@users.noreply.github.com>
2022-01-07 23:36:51 +0530

    fb login

--- app/src/main/java/com/myapp/MainActivity.kt
+++ app/src/main/java/com/myapp/MainActivity.kt
@@ -1,12 +1,72 @@
 package com.myapp
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+       var callbackManager = CallbackManager.Factory.create();
+
+
+        val accessToken = AccessToken.getCurrentAccessToken()
+        val isLoggedIn = accessToken != null && !accessToken.isExpired
+
+        val EMAIL = "email"
+
+        var  loginButton = findViewById<View>(R.id.login_button) as LoginButton
+        loginButton.setReadPermissions(Arrays.asList(EMAIL))
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+
+//        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+        // Callback registration
+        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+
+
+            override fun onCancel() {
+                // App code
+            }
+
+            override fun onError(exception: FacebookException) {
+                // App code
+            }
+
+            override fun onSuccess(result: LoginResult?) {
+
+            }
+        })
+
+
+//        try {
+//            val info =
+//                packageManager.getPackageInfo("com.myapp", PackageManager.GET_SIGNATURES)
+//            for (signature in info.signatures) {
+//                val md: MessageDigest = MessageDigest.getInstance("SHA")
+//                md.update(signature.toByteArray())
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+//            }
+//        } catch (e: PackageManager.NameNotFoundException) {
+//            e.printStackTrace()
+//        } catch (e: NoSuchAlgorithmException) {
+//            e.printStackTrace()
+//        }
     }
+
 }
